7adeb48bac6061e11a22019e73a919da22e6caad
Elliot <ewldeharo@gmail.com>
2026-01-13 12:32:15 +0000

    Update footer copyright year to 2026

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; 2024 Landmark Energy. All rights reserved.</p>
+        <p>&copy; 2026 Landmark Energy. All rights reserved.</p>
       </div>
     </footer>
   )
